98090feda7e2cedf1cf9c4883bc979a1ff6023e8
aschombe <aschombe@stevens.edu>
2024-02-17 19:12:18 -0500

    Organized repo

---
+++ extraction/extract_clubs.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+import re
+
+count = 0
+with open('clubs.txt', 'w') as clubs:
+    with open('List Of SA groups.html') as file:
+        soup = BeautifulSoup(file, 'html.parser')
+        for label in soup.find_all('label'):
+            if label.get('for') and label.get('for').startswith('cb_club'):
+                regex = re.compile(r'Select (.+?)\'s group')
+                match = regex.search(label.get_text())
+                print(match.group(1))
+                count += 1
+                clubs.write(match.group(1) + '\n')
+
+            
+
+print(count)
